978b61002526087d8850e227a11e3e35b3d683c2
JaredDmz <jrd22dmz@hotmail.com>
2019-07-10 17:11:43 -0500

    Update SMSIFacade.php

--- src/SMSIFacade.php
+++ src/SMSIFacade.php
@@ -5,16 +5,16 @@ namespace JaredDmz\LaravelInfobipSMS;
 class SMSIFacade
 {
     public static function enviar($telefono,$mensaje){
-    	if (strlen($telefono) == 10 && $mensaje != '') {
-          $username = env(SMS_USERNAME,false);
-          $password = env(SMS_PASSWORD,false);
+    	if (strlen($telefono) == 12 && $mensaje != '') {
+          $username = env('SMS_USERNAME',false);
+          $password = env('SMS_PASSWORD',false);
 	        if($username != false && $password != false) {
                 $curl = curl_init();
                 curl_setopt_array($curl, array(
                   CURLOPT_URL => "https://nn6x8.api.infobip.com/sms/2/text/single",
                   CURLOPT_RETURNTRANSFER => true, CURLOPT_ENCODING => "", CURLOPT_MAXREDIRS => 10, CURLOPT_TIMEOUT => 30,
                   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, CURLOPT_CUSTOMREQUEST => "POST",
-                  CURLOPT_POSTFIELDS => '{"from":"ALIVIANATE","to":"52'.$telefono.'","text":"'.$mensaje.'"}',
+                  CURLOPT_POSTFIELDS => '{"from":"ALIVIANATE","to":"'.$telefono.'","text":"'.$mensaje.'"}',
                   CURLOPT_USERPWD => ($username . ":" . $password),
                   CURLOPT_HTTPHEADER => array(
                     "accept: application/json",
@@ -31,7 +31,7 @@ class SMSIFacade
                 return ['estatus'=>'no','mensaje'=>'Agrega tus variables de sesión en el archivo .env de la aplicación.'];
             }
 	    }else{
-	    	return ['estatus'=>'no','mensaje'=>'El teléfono debe estar a 10 dígitos y el mensaje con algo escrito.'];
+	    	return ['estatus'=>'no','mensaje'=>'El teléfono debe estar a 12 dígitos y el mensaje con algo escrito.'];
 	    }
     }
 }
